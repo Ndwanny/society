b39f8336f978256585ce9810de95cb8f7cb5ead4
kondwani13 <sakalakhondwani1@gmail.com>
2026-04-01 11:53:40 +0200

    Fix build.sh: remove --no-wasm-dry-run flag (not in Flutter 3.22)

--- build.sh
+++ build.sh
@@ -11,6 +11,6 @@ export PATH="$HOME/flutter/bin:$PATH"
 flutter config --no-analytics
 flutter pub get
 
-flutter build web --release --no-wasm-dry-run \
+flutter build web --release \
   --dart-define=SUPABASE_URL="${SUPABASE_URL}" \
   --dart-define=SUPABASE_ANON_KEY="${SUPABASE_ANON_KEY}"
